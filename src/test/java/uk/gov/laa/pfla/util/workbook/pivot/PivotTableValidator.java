8c99c1816f61a57f1ba62cee901c700770de2ed9
robert.buczek <robert.buczek@digital.justice.gov.uk>
2025-04-24 15:38:00 +0100

    [LPF-818]: Zip bomb detected when processing Excel file in WorkbookUtil

---
+++ src/test/java/uk/gov/laa/pfla/util/workbook/pivot/PivotTableValidator.java
@@ -0,0 +1,37 @@
+package uk.gov.laa.pfla.util.workbook.pivot;
+
+import org.apache.poi.xssf.usermodel.XSSFPivotTable;
+
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationStrategy.CacheDefinitionValidation;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationStrategy.DataSourceValidation;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationStrategy.LocationValidation;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationStrategy.PivotFieldsValidation;
+
+/**
+ * Validates an XSSF PivotTable against a set of predefined validation strategies.
+ * <p>
+ * This validator performs multiple checks on a pivot table to ensure its structural integrity,
+ * including cache definition, data source, location, and field configuration validations.
+ * The validations are performed in an unspecified order, and all validations are executed
+ * regardless of previous failures (unless an exception is thrown).
+ * </p>
+ */
+public class PivotTableValidator {
+
+    private static final PivotTableValidationStrategy[] strategies = {
+            new CacheDefinitionValidation(),
+            new DataSourceValidation(),
+            new LocationValidation(),
+            new PivotFieldsValidation()
+    };
+
+    /**
+     * Validates the specified pivot table against all configured validation strategies.
+     *
+     * @param pivotTable the pivot table to validate (must not be {@code null})
+     */
+    public void validate(XSSFPivotTable pivotTable) {
+        for (var strategy : strategies)
+            strategy.validate(pivotTable);
+    }
+}
