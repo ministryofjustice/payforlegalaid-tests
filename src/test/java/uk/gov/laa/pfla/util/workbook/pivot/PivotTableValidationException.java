89925667d24b2799700850b54a27bde8d1a7657c
Chris Cook <114592674+ChrisCookOC@users.noreply.github.com>
2025-08-04 10:44:52 +0100

    feature: LPF-960 - update AT coverage of pivot tables (#65)
    * LPF-960 - update CCMS invocie analysis data
    
    LPF-960 - commit test
    
    LPF-960 - undo commit test
    
    LPF-960 - update expected response to have data
    
    * LPF-960: code review comments and tidy-up
    LPF-960 - new pivot checks
    
    LPF-960 - remove blank line added by mistake
    
    LPF-960 - remove star import
    
    LPF-960 - refactor to use streams to simplify
    
    LPF-960 - clean up pivot exception formatting
    
    LPF-960 - tighten up number conversions
    
    LPF-960 - avoid forEach throwing exceptions
    
    LPF-960 - avoid forEach throwing exceptions
    
    LPF-960 - remove unused function
    
    LPF-960 - cleanup some tests
    
    LPF-960 - avoid internal function
    
    LPF-960 - put back some old data
    
    LPF-960 - put back some old data

--- src/test/java/uk/gov/laa/pfla/util/workbook/pivot/PivotTableValidationException.java
+++ src/test/java/uk/gov/laa/pfla/util/workbook/pivot/PivotTableValidationException.java
@@ -5,6 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.apache.poi.ooxml.POIXMLDocumentPart;
 
 import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.INVALID_CACHE;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.INVALID_CACHE_FIELD_STYLES;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.INVALID_CACHE_RECORD_RELATIONSHIP;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.INVALID_COLUMN_REFERENCE;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.INVALID_FIELD_STYLES;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.INVALID_SOURCE_SHEET;
+import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.NO_CACHE_FIELDS;
 import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.NO_DATA_SOURCE;
 import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.NO_FIELDS;
 import static uk.gov.laa.pfla.util.workbook.pivot.PivotTableValidationException.ErrorCase.NO_LOCATION;
@@ -64,6 +70,51 @@ public abstract class PivotTableValidationException extends RuntimeException {
     }
 
     /**
+     * Creates a validation exception for a pivot table where the pivot cache and pivot cache records are not properly
+     * linked together.
+     */
+    public static PivotTableValidationException invalidCacheRecordsRelationship(POIXMLDocumentPart pivotTable) {
+        return new PivotTableValidationException(INVALID_CACHE_RECORD_RELATIONSHIP, pivotTable) {
+        };
+    }
+
+    /**
+     * Creates a validation exception for a pivot table where the configured fields are referencing invalid styles.
+     */
+    public static PivotTableValidationException invalidFieldStyle(POIXMLDocumentPart pivotTable) {
+        return new PivotTableValidationException(INVALID_FIELD_STYLES, pivotTable) {
+        };
+    }
+
+    /**
+     * Creates a validation exception for a pivot table where the configured cache fields are referencing invalid styles.
+     */
+    public static PivotTableValidationException invalidCacheFieldStyle(POIXMLDocumentPart pivotTable) {
+        return new PivotTableValidationException(INVALID_CACHE_FIELD_STYLES, pivotTable) {};
+    }
+
+    /**
+     * Creates a validation exception for a pivot table where the source sheet is invalid.
+     */
+    public static PivotTableValidationException invalidSourceSheet(POIXMLDocumentPart pivotTable) {
+        return new PivotTableValidationException(INVALID_SOURCE_SHEET, pivotTable) {};
+    }
+
+    /**
+     * Creates a validation exception for a pivot table definition uses columns that no longer exist.
+     */
+    public static PivotTableValidationException invalidColumnReference(POIXMLDocumentPart pivotTable) {
+        return new PivotTableValidationException(INVALID_COLUMN_REFERENCE, pivotTable) {};
+    }
+
+    /**
+     * Creates a validation exception for a pivot table where the cache has no records and so is invalid.
+     */
+    public static PivotTableValidationException noCachedFields(POIXMLDocumentPart pivotTable) {
+        return new PivotTableValidationException(NO_CACHE_FIELDS, pivotTable) {};
+    }
+
+    /**
      * Defines the specific validation failure cases for pivot tables.
      */
     @RequiredArgsConstructor
@@ -71,7 +122,13 @@ public abstract class PivotTableValidationException extends RuntimeException {
         INVALID_CACHE("Pivot table at %s has invalid cache definition"),
         NO_DATA_SOURCE("Pivot table at %s has no data source reference"),
         NO_LOCATION("Pivot table at %s has no defined location"),
-        NO_FIELDS("Pivot table at %s has no configured fields");
+        NO_FIELDS("Pivot table at %s has no configured fields"),
+        INVALID_CACHE_RECORD_RELATIONSHIP("Pivot table at %s has a pivot cache which does not link to the pivot cache records file"),
+        INVALID_FIELD_STYLES("Pivot table at %s has pivot fields with invalid style references"),
+        INVALID_CACHE_FIELD_STYLES("Pivot table at %s has pivot cache fields with invalid style references"),
+        INVALID_SOURCE_SHEET("Pivot table at %s is set to use a sheet that does not exist"),
+        INVALID_COLUMN_REFERENCE("Pivot table at %s is referencing a data column that doesn't exist"),
+        NO_CACHE_FIELDS("Pivot table at %s has no cached fields");
 
         private final String messageFormat;
 
